350d5279a1dc9232dca8ed6f36babc0ddcc86903
Michał Dulski <michal.dulski@efitness.com>
2020-09-04 16:19:08 +0200

    a

--- WebApplication1/Program.cs
+++ WebApplication1/Program.cs
@@ -13,7 +13,7 @@ namespace WebApplication1
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("Version 111aatest");
+            Console.WriteLine("Version 1111aatest");
             CreateHostBuilder(args).Build().Run();
         }
 
