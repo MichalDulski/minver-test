a7e8a2b4df17406b97b18dc400371fbbafdc8af7
Michał Dulski <michal.dulski@efitness.com>
2020-09-04 15:36:40 +0200

    init

---
+++ VersionControlTestProj/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace VersionControlTestProj
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Heaaallo World!");
+        }
+    }
+}
