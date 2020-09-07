f7b5faba3ef50c465477a247beffb34365127ea2
Michał Dulski <michal.dulski@efitness.com>
2020-09-07 09:41:48 +0200

    cake is a lie

--- WebApplication1/Program.cs
+++ WebApplication1/Program.cs
@@ -15,7 +15,7 @@ namespace WebApplication1
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("Init1");
+            Console.WriteLine("Init 1");
             Assembly assembly = Assembly.GetExecutingAssembly();
             FileVersionInfo fileVersionInfo = FileVersionInfo.GetVersionInfo(assembly.Location);
             string version = fileVersionInfo.ProductVersion;
