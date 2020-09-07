74e3e1680b6ec50ca38aeaf8583aa323df1728e7
Michał Dulski <michal.dulski@efitness.com>
2020-09-07 09:34:11 +0200

    commit 2

--- WebApplication1/obj/Debug/netcoreapp3.1/WebApplication1.AssemblyInfo.cs
+++ WebApplication1/obj/Debug/netcoreapp3.1/WebApplication1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("0.0.2.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("0.0.2-alpha.0.1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("0.0.2-alpha.0.2")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("0.0.0.0")]
