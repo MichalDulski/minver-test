a351be37c9fbff7d91e766a560a18df568b4e804
Kamil Krzyszowski <kamil.krzyszowski@efitness.com>
2020-10-07 15:23:45 +0200

    test 4

--- ConsoleApp2/Program.cs
+++ ConsoleApp2/Program.cs
@@ -6,7 +6,7 @@ namespace ConsoleApp2
 	{
 		static void Main(string[] args)
 		{
-			Console.WriteLine("Hello World!"); // :thinkingemoji: hmmmtrrr
+			Console.WriteLine("Hello World!"); // :thinkingemoji: hmmmtrrrt
 		}
 	}
 }
