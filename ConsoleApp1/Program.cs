915c84da0593439793d16cdae81a44924ab2e9bb
Kamil Krzyszowski <kamil.krzyszowski@efitness.com>
2020-10-08 14:15:39 +0200

    t

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -6,7 +6,7 @@ namespace ConsoleApp1
 	{
 		static void Main(string[] args)
 		{
-            Console.WriteLine("Hello World!"); // hmmmmmmmmmttttyyyggtt
+            Console.WriteLine("Hello World!"); // hmmmmmmmmmttttyyyggttg
 		}
 	}
 }
